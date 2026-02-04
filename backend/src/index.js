666548103c7ee5c92cf9f7c623ebc047e91f0c26
Djemilatou Bonkoungou <bonkougoutoussiane.com>
2026-02-04 03:45:13 +0000

    fix: add localhost:5173 and Vercel URL to CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:8080", "http://localhost:8081"],
+  origin: ["http://localhost:5173","http://localhost:8080", "http://localhost:8081","https://languagecoach-rfjy.onrender.com","hhttps://coach-ai-rwet.vercel.app/" ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
